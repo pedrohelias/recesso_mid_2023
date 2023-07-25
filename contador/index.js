b6e0dbc82fb880196f88201e217c448a47277d7d
pedrohelias <pedrohelias95@hotmail.com>
2023-07-25 12:53:02 -0300

    using css

--- contador/index.js
+++ contador/index.js
@@ -3,18 +3,45 @@ let remove = document.getElementById("buttonSub");
 let reset = document.getElementById("buttonReset");
 let number = document.getElementById("number");
 let value = 0;
+number.style.color = "blue"
 
 add.addEventListener("click", function(){
     value++;
+    if(checkParity(value) == "par"){
+        number.style.color = "blue"
+    }else{
+        number.style.color = "red"
+    }
     number.innerHTML = value;
 })
 
 remove.addEventListener("click", function(){
     value--;
+    if(checkParity(value) == "par"){
+        number.style.color = "blue"
+    }else{
+        number.style.color = "red"
+    }
     number.innerHTML = value;
 })
 
 reset.addEventListener("click", function(){
     value = 0;
+    if(checkParity(value) == "par"){
+        number.style.color = "blue"
+    }else{
+        number.style.color = "red"
+    }
     number.innerHTML = value;
 })
+
+
+function checkParity(value){
+    let calculus = value%2
+    if(calculus == 0){
+        return "par"
+    }else{
+        return "impar"
+    }
+
+}
